612775b43e64599df4b68a011b2a730e27dd6b01
EmilTheApril <pjuske.2002@hotmail.com>
2023-11-15 15:25:29 +0100

    detect

--- CameraObjectDetection.py
+++ CameraObjectDetection.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import json
+from PIL import Image
 
 #Pointer to webcam
 video = cv2.VideoCapture(0)
@@ -8,7 +9,7 @@ video = cv2.VideoCapture(0)
 #Counter
 i = 0
 
-while True:
+while i < 3:
     #Turns on camera and saves the image in img
     success, img = video.read()
     
@@ -26,6 +27,39 @@ while True:
     
     #Enlargens all pixels
     erodeImage = cv2.erode(threshold, kernel3x3)
+    
+    #Bloblist holds all pixel pos
+    blobList = [[]]
+    arrayBlobCount = 0
+    count = 0
+    print(erodeImage.shape)
+    width, height = 670, 1030
+    
+    if i == 2:
+        for y in range(height):
+            for x in range(width):
+                print(erodeImage[x, y])
+                if erodeImage[x, y] == 255:
+                    print("Yes")
+                    blobList[arrayBlobCount].append([x, y])
+                    
+                    count = 0
+                    while count < len(blobList[arrayBlobCount]):
+                        pixelCheck = []
+                        if blobList[arrayBlobCount][count][0] + 1 <= width & blobList[arrayBlobCount][count][1] + 1 <= height:
+                            pixelCheck.append(erodeImage[blobList[arrayBlobCount][count][0] + 1, blobList[arrayBlobCount][count][1] + 1])
+                        if blobList[arrayBlobCount][count][0] + 1 <= width & blobList[arrayBlobCount][count][1] + 1 >= 0:
+                            pixelCheck.append(erodeImage[blobList[arrayBlobCount][count][0] + 1, blobList[arrayBlobCount][count][1] - 1])
+                        if blobList[arrayBlobCount][count][0] + 1 >= 0 & blobList[arrayBlobCount][count][1] + 1 <= height:
+                            pixelCheck.append(erodeImage[blobList[arrayBlobCount][count][0] - 1, blobList[arrayBlobCount][count][1] + 1])
+                        if blobList[arrayBlobCount][count][0] + 1 >= 0 & blobList[arrayBlobCount][count][1] + 1 >= 0:
+                            pixelCheck.append(erodeImage[blobList[arrayBlobCount][count][0] - 1, blobList[arrayBlobCount][count][1] - 1])
+                        for i in range(len(pixelCheck)):
+                            if pixelCheck[i] == 255:
+                                blobList[arrayBlobCount].append(pixelCheck[i])
+                        count += 1
+                            
+        print(blobList[0])
     
     #Shows the image/video
     cv2.imshow("webcam", erodeImage)
